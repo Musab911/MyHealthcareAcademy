02f5ee25f4861a8cf9946c06f7370d8d492b389f
Musab911 <110714635+Musab911@users.noreply.github.com>
2024-07-15 16:00:46 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\admin;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 //Route::get('/intern', [admin::class, 'index']);
